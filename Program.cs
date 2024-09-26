e7f933f2925ac122ffead3f9193f2db35534f933
Bunyod Mukhammadiev <bunyoddil32@gmail.com>
2024-09-26 10:56:11 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿System.Console.WriteLine("Enter your name;");
+string name = Console.ReadLine();
+string greeting = $"Hello, {name}";
+System.Console.WriteLine(greeting);
+
+System.Console.Write("Enter your age:");
+string ageAsString = Console.ReadLine();
+System.Console.WriteLine(ageAsString);
+int age = Convert.ToInt32(ageAsString);
+System.Console.WriteLine($"Successfully converted! {age}");
+int keschasAge = 3;
+int ageDifference = age - keschasAge;
+System.Console.WriteLine($"The difference between your and Kesche's age is {ageDifference}");
+
+if(age > keschasAge)
+{
+    System.Console.WriteLine("You are older!");
+    
+}
+else if(age==keschasAge)
+{
+    System.Console.WriteLine("You are youngar!");
+
+}
